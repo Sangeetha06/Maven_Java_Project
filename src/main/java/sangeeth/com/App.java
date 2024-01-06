446d7ea10cbe1c5ea7565fdeab3be454c8da0c1f
Sangeetha Gnanasekaren <sangeethagsekaren@gmail.com>
2024-01-06 12:20:00 +0530

    Add files via upload

---
+++ src/main/java/sangeeth/com/App.java
@@ -0,0 +1,13 @@
+package sangeeth.com;
+
+/**
+ * This is a sample Maven Java Project!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is a sample Maven Java Project!" );
+    }
+}
